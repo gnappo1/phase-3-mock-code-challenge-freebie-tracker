fa252890b65fb2aec035900f7e6e005a589145bb
Matteo Piccini <gnappo.1@live.it>
2023-01-17 10:25:23 -0800

    Complete first aggregate and association method

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
-
+    has_many :freebies #16/17
+    has_many :devs, through: :freebies
 end
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,8 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :dev # #dev/#dev=
+    belongs_to :company # company, company=
 
+    def print_details
+        "#{dev.name} owns a #{item_name} from #{company.name}"
+    end
 end
